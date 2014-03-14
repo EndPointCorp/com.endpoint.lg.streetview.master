54124c54c8b0bd3dee6d95c0e945f7fe706ff003
Matt Vollrath <matt@endpoint.com>
2014-03-14 02:27:56 -0400

    Split Street View activity
    * Create separate streetview.master activity
    * Move state authority to streetview.master
    * Move input handling to streetview.master
    * Use WebConfigHandler in streetview.pano
    * Streamline streetview.pano webapp
    * Revisit streetview.pano configuration keys
    * Remove much code

---
+++ src/main/java/com/endpoint/lg/streetview/master/StreetviewMasterActivity.java
@@ -0,0 +1,246 @@
+/*
+ * Copyright (C) 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.endpoint.lg.streetview.master;
+
+import interactivespaces.activity.impl.ros.BaseRoutableRosActivity;
+import interactivespaces.util.data.json.JsonNavigator;
+
+import com.endpoint.lg.support.evdev.InputKeyEvent;
+
+import com.endpoint.lg.support.message.RosMessageHandler;
+import com.endpoint.lg.support.message.streetview.MessageTypesStreetview;
+import com.endpoint.lg.support.message.RosMessageHandlers;
+
+import java.util.Map;
+
+import com.endpoint.lg.support.evdev.InputAbsState;
+import com.endpoint.lg.support.evdev.InputEventCodes;
+import com.endpoint.lg.support.domain.streetview.StreetviewLinks;
+import com.endpoint.lg.support.domain.streetview.StreetviewPov;
+import com.endpoint.lg.support.domain.streetview.StreetviewPano;
+
+/**
+ * An activity responsible for authoritative Street View state, input handling,
+ * and persistence.
+ * 
+ * <p>
+ * Activity states:
+ * 
+ * <p>
+ * RUNNING : State is receiving updates and responding to refresh requests.
+ * 
+ * <p>
+ * ACTIVE : Input events are being handled.
+ * 
+ * @author Matt Vollrath <matt@endpoint.com>
+ */
+public class StreetviewMasterActivity extends BaseRoutableRosActivity {
+
+  /**
+   * Coefficient of input event value to POV translation.
+   */
+  public static final double INPUT_SENSITIVITY = 0.0032;
+
+  /**
+   * How much "momentum" on a controller is needed to move forward or backward.
+   */
+  public static final int INPUT_MOVEMENT_COUNT = 10;
+
+  /**
+   * Controller forward/backward axes must exceed this value for movement (after
+   * sensitivity).
+   */
+  public static final double INPUT_MOVEMENT_THRESHOLD = 1.0;
+
+  /**
+   * After movement, wait this many milliseconds before moving again.
+   */
+  public static final int INPUT_MOVEMENT_COOLDOWN = 250;
+
+  private StreetviewModel model;
+
+  private RosMessageHandlers rosHandlers;
+
+  long lastMoveTime;
+  int movementCounter;
+
+  /**
+   * Initialize movement state.
+   */
+  private void initMovement() {
+    lastMoveTime = System.currentTimeMillis();
+    movementCounter = 0;
+  }
+
+  /**
+   * Sends incoming Ros messages to the Ros message handlers.
+   */
+  @Override
+  public void onNewInputJson(String channel, Map<String, Object> message) {
+    rosHandlers.handleMessage(channel, message);
+  }
+
+  /**
+   * Broadcast the current point of view.
+   */
+  private void broadcastPov() {
+    sendOutputJsonBuilder(MessageTypesStreetview.MESSAGE_TYPE_STREETVIEW_POV, model.getPov()
+        .getJsonBuilder());
+  }
+
+  /**
+   * Broadcast the current panorama.
+   */
+  private void broadcastPano() {
+    sendOutputJsonBuilder(MessageTypesStreetview.MESSAGE_TYPE_STREETVIEW_PANO, model.getPano()
+        .getJsonBuilder());
+
+    initMovement(); // reset movement state on pano change
+  }
+
+  /**
+   * Initialize state, register message handlers.
+   */
+  @Override
+  public void onActivitySetup() {
+    model = new StreetviewModel();
+
+    initMovement();
+
+    rosHandlers = new RosMessageHandlers(getLog());
+
+    // handles link updates from the browser
+    rosHandlers.registerHandler(MessageTypesStreetview.MESSAGE_TYPE_STREETVIEW_LINKS,
+        new RosMessageHandler() {
+          public void handleMessage(JsonNavigator json) {
+            model.setLinks(new StreetviewLinks(json));
+          }
+        });
+
+    // handle external pov updates
+    rosHandlers.registerHandler(MessageTypesStreetview.MESSAGE_TYPE_STREETVIEW_POV,
+        new RosMessageHandler() {
+          public void handleMessage(JsonNavigator json) {
+            model.setPov(new StreetviewPov(json));
+          }
+        });
+
+    // handle external pano updates
+    rosHandlers.registerHandler(MessageTypesStreetview.MESSAGE_TYPE_STREETVIEW_PANO,
+        new RosMessageHandler() {
+          public void handleMessage(JsonNavigator json) {
+            model.setPano(new StreetviewPano(json));
+          }
+        });
+
+    // handle refresh requests
+    rosHandlers.registerHandler(MessageTypesStreetview.MESSAGE_TYPE_STREETVIEW_REFRESH,
+        new RosMessageHandler() {
+          public void handleMessage(JsonNavigator json) {
+            if (model.getPano() != null)
+              broadcastPano();
+            if (model.getPov() != null)
+              broadcastPov();
+          }
+        });
+
+    // handle button events, if activated
+    rosHandlers.registerHandler("EV_KEY", new RosMessageHandler() {
+      public void handleMessage(JsonNavigator json) {
+        if (isActivated())
+          onRosKeyEvent(new InputKeyEvent(json));
+      }
+    });
+
+    // handle absolute axis state changes, if activated
+    rosHandlers.registerHandler("EV_ABS", new RosMessageHandler() {
+      public void handleMessage(JsonNavigator json) {
+        if (isActivated())
+          onRosAbsStateChange(new InputAbsState(json));
+      }
+    });
+  }
+
+  /**
+   * Handle an EV_KEY event.
+   * 
+   * @param event
+   *          the button event
+   */
+  private void onRosKeyEvent(InputKeyEvent event) {
+    if (event.getValue() > 0) {
+      if (event.getCode() == InputEventCodes.BTN_0 && model.moveForward()) {
+        broadcastPano();
+        getLog().info("Moved Street View forward");
+      }
+
+      if (event.getCode() == InputEventCodes.BTN_1 && model.moveBackward()) {
+        broadcastPano();
+        getLog().info("Moved Street View backward");
+      }
+    }
+  }
+
+  /**
+   * Handle an EV_ABS state update.
+   * 
+   * @param state
+   *          the axis state
+   */
+  private void onRosAbsStateChange(InputAbsState state) {
+    double yaw = state.getValue(InputEventCodes.ABS_RZ) * INPUT_SENSITIVITY;
+
+    if (yaw != 0) {
+      model.getPov().translate(yaw, 0);
+
+      broadcastPov();
+    }
+
+    long currentTime = System.currentTimeMillis();
+
+    // movement can be either forwards or backwards, depending on whether the
+    // SpaceNav is moved+tilted forwards or backwards.
+    // TODO: Movement in all directions.
+    double movement =
+        -INPUT_SENSITIVITY
+            * ((state.getValue(InputEventCodes.ABS_Y) + state.getValue(InputEventCodes.ABS_RX)));
+
+    if (movement > INPUT_MOVEMENT_THRESHOLD) {
+      movementCounter++;
+    } else if (movement < -INPUT_MOVEMENT_THRESHOLD) {
+      movementCounter--;
+    } else {
+      movementCounter = 0;
+    }
+
+    if ((currentTime - lastMoveTime) < INPUT_MOVEMENT_COOLDOWN) {
+      movementCounter = 0;
+    } else if (movementCounter > INPUT_MOVEMENT_COUNT && model.moveForward()) {
+      broadcastPano();
+    } else if (movementCounter < -INPUT_MOVEMENT_COUNT && model.moveBackward()) {
+      broadcastPano();
+    }
+  }
+
+  /**
+   * Re-initialize movement state on activation.
+   */
+  @Override
+  public void onActivityActivate() {
+    initMovement();
+  }
+}
---
+++ src/main/java/com/endpoint/lg/streetview/master/StreetviewModel.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.endpoint.lg.streetview.master;
+
+import com.endpoint.lg.support.domain.streetview.StreetviewLink;
+
+import com.endpoint.lg.support.domain.streetview.StreetviewLinks;
+import com.endpoint.lg.support.domain.streetview.StreetviewPano;
+import com.endpoint.lg.support.domain.streetview.StreetviewPov;
+
+/**
+ * A model representing the Street View state.
+ * 
+ * <p>
+ * The model keeps track of whether or not its links are up-to-date. If the pano
+ * is changed, the links are marked dirty and remain dirty until set. This helps
+ * prevent navigation to stale links.
+ * 
+ * @author Matt Vollrath <matt@endpoint.com>
+ */
+public class StreetviewModel {
+  private StreetviewPano pano;
+  private StreetviewPov pov;
+  private StreetviewLinks links;
+
+  private boolean linksDirty;
+
+  public StreetviewPano getPano() {
+    return pano;
+  }
+
+  /**
+   * Set the current panorama
+   * 
+   * @param pano
+   *          the pano
+   * @return true if the pano changed
+   */
+  public boolean setPano(StreetviewPano pano) {
+    if (this.pano == null || !this.pano.equals(pano)) {
+      this.pano = pano;
+      linksDirty = true;
+      return true;
+    }
+
+    return false;
+  }
+
+  public StreetviewPov getPov() {
+    return pov;
+  }
+
+  public void setPov(StreetviewPov pov) {
+    this.pov = pov;
+  }
+
+  public StreetviewLinks getLinks() {
+    return links;
+  }
+
+  public void setLinks(StreetviewLinks links) {
+    this.links = links;
+    linksDirty = false;
+  }
+
+  public StreetviewModel() {
+    linksDirty = true;
+  }
+
+  /**
+   * Move to a neighboring panorama nearest to the given direction.
+   * 
+   * @param heading
+   *          direction to move
+   * @return true if the pano changed
+   */
+  public boolean moveToward(double heading) {
+    if (linksDirty)
+      return false; // this also prevents reading null links
+
+    StreetviewLink nearest = links.getNearestLink(heading);
+
+    if (nearest != null) {
+      return setPano(new StreetviewPano(nearest.getPano()));
+    }
+
+    return false;
+  }
+
+  /**
+   * Move to a neighboring panorama nearest the current heading.
+   * 
+   * @return true if the pano changed
+   */
+  public boolean moveForward() {
+    if (pov != null)
+      return moveToward(pov.getHeading());
+
+    return false;
+  }
+
+  /**
+   * Move to a neighboring panorama farthest from the current heading.
+   * 
+   * @return true if the pano changed
+   */
+  public boolean moveBackward() {
+    if (pov != null)
+      return moveToward(pov.getHeading() - 180);
+
+    return false;
+  }
+}
